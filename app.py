8a9b18d8383b7ed55a680161c1b9b250308b2e7d
zyaj <zyaj@stanford.edu>
2017-08-25 14:37:22 -0700

    add visualization with d3

--- app.py
+++ app.py
@@ -14,14 +14,12 @@ bv = bokeh.__version__
 # start coding
 app = Flask(__name__)
 app.vars={}
-feat = ['Open','Close','Range']
-
+#feat = ['Open','Close','Range']
 
 # initial page
 @app.route('/')
 def main():
 	return redirect('/index')
-
 
 @app.route('/index',methods=['GET','POST'])
 def index():
@@ -35,6 +33,7 @@ def index():
 		app.vars['start_year'] = request.form['year']
 		app.vars['start_month'] = request.form['month']
 		app.vars['timespan'] = request.form['timespan']
+		app.vars['desc']= '%s Price' % app.vars['ticker']
 		try: 
 			int(app.vars['start_year'])
 			app.vars['tag_year'] = 'Start year specified as year %s' % app.vars['start_year']
@@ -49,31 +48,36 @@ def index():
 			app.vars['tag_month'] = 'Start month not specified/recognized'
 			app.vars['timespan'] = ''
 			app.vars['tag_span'] = 'Time span not specified/recognized'
-		app.vars['select'] = [feat[q] for q in range(3) if feat[q] in request.form.values()]
+		#app.vars['select'] = [feat[q] for q in range(3) if feat[q] in request.form.values()]
 		return redirect('/graph')
 
-
-@app.route('/graph',methods=['GET','POST'])
-def graph():
+# get data
+@app.route("/data")
+def data():
   # get stock data
 	req = 'https://www.quandl.com/api/v3/datasets/WIKI/'
 	req = '%s%s.json?&collapse=weekly' % (req,app.vars['ticker'])
 	if app.vars['start_year']!='':
 		req = '%s&start_date=%s-01-01' % (req,app.vars['start_year'])
-	r = requests.get(req)
-
-	# transfer json data to date frame
-	start_date=datetime.date(year=int(app.vars['start_year']),day=1,month=int(app.vars['start_month']));
-	end_date=datetime.date(year=int(app.vars['start_year'])+int(app.vars['timespan']),day=1,month=int(app.vars['start_month']));
+	r = requests.get(req)	
 	cols = r.json()['dataset']['column_names'][0:5]
 	df = pd.DataFrame(np.array(r.json()['dataset']['data'])[:,0:5],columns=cols)
 	df.Date = pd.to_datetime(df.Date)
-	df[['Open','High','Low','Close']] = df[['Open','High','Low','Close']].astype(float)
+	df[['Open','High','Low','Close']] = df[['Open','High','Low','Close']].astype(float) 
+	df.columns=['date','Open','High','Low','Close'];
 	if app.vars['start_year']!='':
-		if df.Date.iloc[-1].year>int(app.vars['start_year']):
-			app.vars['tag_year'] = '%s, but Quandl record begins in %s' % (app.vars['tag_year'],df.Date.iloc[-1].year)
-	app.vars['desc'] = r.json()['dataset']['name'].split(',')[0]
-	
+		if df.date.iloc[-1].year>int(app.vars['start_year']):
+			app.vars['tag_year'] = '%s, but Quandl record begins in %s' % (app.vars['tag_year'],df.date.iloc[-1].year)
+	#app.vars['desc'] = r.json()['dataset']['name'].split(',')[0]  
+	return df.to_csv(index=False,sep="\t")
+
+@app.route('/graph',methods=['GET','POST'])
+def graph():
+	'''
+	# transfer json data to date frame
+	start_date=datetime.date(year=int(app.vars['start_year']),day=1,month=int(app.vars['start_month']));
+	end_date=datetime.date(year=int(app.vars['start_year'])+int(app.vars['timespan']),day=1,month=int(app.vars['start_month']));
+
   # plots
 	p = figure(plot_width=450, plot_height=450, title=app.vars['ticker'], x_axis_type="datetime",x_range=Range1d(start_date,end_date))
 	if 'Range' in app.vars['select']:
@@ -94,14 +98,11 @@ def graph():
 	p.yaxis.axis_label = "Stock price ($)"
 	p.yaxis.axis_label_text_font_style = 'bold'
 	p.yaxis.axis_label_text_font_size = '16pt'
-	p.yaxis.major_label_text_font_size = '14pt'
-	
-	# render graph template
-	# ------------------- ------------------------|
+	p.yaxis.major_label_text_font_size = '14pt'	
 	script, div = components(p)
-	return render_template('graph.html', bv=bv, ticker=app.vars['ticker'],
-							ttag=app.vars['desc'], yrtag=app.vars['tag_year'],
-							script=script, div=div)
+	'''
+	return render_template('graph.html', ticker=app.vars['ticker'],
+							ttag=app.vars['desc'], yrtag=app.vars['tag_year'])
 		
 	
 @app.errorhandler(500)
