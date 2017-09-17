3e0d4e16c1dcfe84e6d4d8143a49b5b927b94d27
zyaj <zyaj@stanford.edu>
2017-09-16 20:19:56 -0700

    recover

---
+++ README.md
@@ -0,0 +1,15 @@
+# Simple flask stock check appicaltion
+
+## Introduction
+This repository applies python-flask to generate a simple stock price check-plot application. 
+The visualization is implemented by d3js. The application needs three input: The ticker of the stock, 
+start year and time span to check. The plot will show all high, low, open and close price each day 
+for the select stock.
+
+## Implementation
+In order to implement locally, just try
+<br align="center">python app.py</br>
+You should see it on localhost:50000.
+
+## Heroku
+The application has been deployed on heroku.com. Please check zhangyun-12day.herokuapp.com.
--- app.py
+++ app.py
@@ -1,70 +1,122 @@
+# import packages
 from flask import Flask, render_template, request, redirect
-import io
-import csv
+import requests
 import numpy as np
-import sys
-import kmeans as kn
-app = Flask(__name__)
+import pandas as pd
+import bokeh
+import datetime
+from bokeh.models import Range1d
+from bokeh.plotting import figure
+from bokeh.io import show
+from bokeh.embed import components
+bv = bokeh.__version__
 
+# start coding
+app = Flask(__name__)
+app.vars={}
+#feat = ['Open','Close','Range']
+
+# initial page
 @app.route('/')
 def main():
 	return redirect('/index')
 
-@app.route('/index',methods=['GET'])
+@app.route('/index',methods=['GET','POST'])
 def index():
-   # page for upload csv data and number of cluster
-   return render_template('index.html')
-	
-@app.route('/result', methods = ['POST'])
-def result():
-   if request.method == 'POST':
-      # ------------------------
-      # load in input data
-      # if error, output error.html
-      # get number of cluster
-      try:
-         c_cnt=int(request.form['c_cnt'])
-         method=int(request.form['method'])
-         n_init=int(request.form['n_init'])
+	# direct to the first page
+	# collect stock name, start year, month and time span
+	if request.method == 'GET':
+		return render_template('index.html')
+	else:
+		#request was a POST
+		app.vars['ticker'] = request.form['ticker'].upper()
+		app.vars['start_year'] = request.form['year']
+		app.vars['start_month'] = request.form['month']
+		app.vars['timespan'] = request.form['timespan']
+		app.vars['desc']= '%s Price' % app.vars['ticker']
+		try: 
+			int(app.vars['start_year'])
+			app.vars['tag_year'] = 'Start year specified as year %s' % app.vars['start_year']
+			int(app.vars['start_month'])
+			app.vars['tag_month'] = 'Start month specified as month %s' % app.vars['start_month']
+			int(app.vars['timespan'])
+			app.vars['tag_span'] = 'Time span specified as %s month' % app.vars['timespan']
+		except ValueError: 
+			app.vars['start_year'] = ''
+			app.vars['tag_year'] = 'Start year not specified/recognized'
+			app.vars['start_month'] = ''
+			app.vars['tag_month'] = 'Start month not specified/recognized'
+			app.vars['timespan'] = ''
+			app.vars['tag_span'] = 'Time span not specified/recognized'
+		#app.vars['select'] = [feat[q] for q in range(3) if feat[q] in request.form.values()]
+		return redirect('/graph')
 
-      except ValueError:
-         error_msg='Wrong input for the number of clusters, initialization method or number of runs.'
-         return render_template('error.html',error_msg=error_msg)
+# get data
+@app.route("/data")
+def data():
+  # get stock data
+	req = 'https://www.quandl.com/api/v3/datasets/WIKI/'
+	req = '%s%s.json?&collapse=weekly' % (req,app.vars['ticker'])
+	if app.vars['start_year']!='':
+		req = '%s&start_date=%s-01-01' % (req,app.vars['start_year'])
+	r = requests.get(req)	
+	cols = r.json()['dataset']['column_names'][0:5]
+	df = pd.DataFrame(np.array(r.json()['dataset']['data'])[:,0:5],columns=cols)
+	df.Date = pd.to_datetime(df.Date)
+	df[['Open','High','Low','Close']] = df[['Open','High','Low','Close']].astype(float) 
+	df.columns=['date','Open','High','Low','Close'];
+	if app.vars['start_year']!='':
+		if df.date.iloc[-1].year>int(app.vars['start_year']):
+			app.vars['tag_year'] = '%s, but Quandl record begins in %s' % (app.vars['tag_year'],df.date.iloc[-1].year)
+	#app.vars['desc'] = r.json()['dataset']['name'].split(',')[0]  
+	return df.to_csv(index=False,sep="\t")
 
-      # get the vector data
-      csvfile = io.TextIOWrapper(request.files['file'].stream, encoding='gbk')
-      reader = csv.reader(csvfile)
-      X=np.array([[float(x) for x in line] for line in reader])
+@app.route('/graph',methods=['GET','POST'])
+def graph():
+	'''
+	# transfer json data to date frame
+	start_date=datetime.date(year=int(app.vars['start_year']),day=1,month=int(app.vars['start_month']));
+	end_date=datetime.date(year=int(app.vars['start_year'])+int(app.vars['timespan']),day=1,month=int(app.vars['start_month']));
 
-      if X.shape[0]==0:
-         error_msg="No node data has been uploaded."
-         return render_template('error.html',error_msg=error_msg)
-
-      if X.shape[0]<c_cnt:
-         error_msg="The number of cluster (k) is bigger than the number of vectors. Try a smaller k."
-         return render_template('error.html',error_msg=error_msg)
-
-      # -----------------
-      # clustering all X
-      kmeans=kn.KMeans(c_cnt,method=method,n_init=n_init)
-      kmeans.fit(X)
-
-      # ------------------
-      # output results
-      # the number of nodes for each cluster
-      c_node_num=np.array([len(nodes) for nodes in kmeans.clusters.nodes])
-      
-      # the ordered cluster center based on the number of nodes
-      centers=np.array([x for _, x in sorted(zip(c_node_num,kmeans.clusters.coords), key=lambda pair: pair[0])])
-      centers=np.round(centers,1)
-      centers=[', '.join(center.astype(str)) for center in centers]
-      # sorted node number for each cluster
-      c_node_num=','.join(sorted(c_node_num.astype(str)))
-      return render_template('result.html',
-         c_cnt=c_cnt,c_node_num=c_node_num,centers=centers,node_num=X.shape[0])
+  # plots
+	p = figure(plot_width=450, plot_height=450, title=app.vars['ticker'], x_axis_type="datetime",x_range=Range1d(start_date,end_date))
+	if 'Range' in app.vars['select']:
+		tmpx = np.array([df.Date,df.Date[::-1]]).flatten()
+		tmpy = np.array([df.High,df.Low[::-1]]).flatten()
+		p.patch(tmpx, tmpy, alpha=0.3, color="gray",legend='Range (High/Low)')
+	if 'Open' in app.vars['select']:
+		p.line(df.Date, df.Open, line_width=2,legend='Opening price')
+	if 'Close' in app.vars['select']:
+		p.line(df.Date, df.Close, line_width=2, line_color="red",legend='Closing price')
 		
+	# axis labels
+	p.xaxis.axis_label = "Date"
+	p.xaxis.axis_label_text_font_style = 'bold'
+	p.xaxis.axis_label_text_font_size = '16pt'
+	p.xaxis.major_label_orientation = np.pi/4
+	p.xaxis.major_label_text_font_size = '14pt'
+	p.yaxis.axis_label = "Stock price ($)"
+	p.yaxis.axis_label_text_font_style = 'bold'
+	p.yaxis.axis_label_text_font_size = '16pt'
+	p.yaxis.major_label_text_font_size = '14pt'	
+	script, div = components(p)
+	'''
+	return render_template('graph.html', ticker=app.vars['ticker'],
+							ttag=app.vars['desc'], yrtag=app.vars['tag_year'])
+		
+	
+@app.errorhandler(500)
+def error_handler(e):
+	return render_template('error.html',ticker=app.vars['ticker'],year=app.vars['start_year'])
+
+    
+# # If main
+# -----------------------------------------------------|
 if __name__ == '__main__':
-   try:
-      app.run(debug = True,port=int(sys.argv[1]))
-   except PermissionError:
-      print('Permission Error! Try different port number, maybe >5000')
+  app.run(port=50000,debug=True)
+  
+  
+  
+#                                    END ALL
+# # ---------------------------------------------------------------------------|
+    
